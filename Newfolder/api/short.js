8f99988d2f3f7f308dd1dd1b1dcf29159bd146e4
dam737 <nguyenvudam12345@gmail.com>
2026-02-18 15:11:57 +0700

    Refactor handler function in short.js

--- Newfolder/api/short.js
+++ Newfolder/api/short.js
@@ -1,21 +1,15 @@
 export default async function handler(req,res){
 
- const url = req.query.url;
+ const url=req.query.url;
 
- if(!url){
-   return res.json({error:"missing url"});
- }
-
- const api =
+ const api=
  "https://yeumoney.com/QL_api.php"+
  "?token=d754503b7998d34db1e601738a0934fca7dbd20a72fdd615ecabcf6a6919a14a"+
  "&format=text"+
  "&url="+encodeURIComponent(url);
 
- const r = await fetch(api);
- const text = await r.text();
+ const r=await fetch(api);
+ const text=await r.text();
 
- res.json({
-   short: text.trim()
- });
+ res.json({short:text.trim()});
 }
