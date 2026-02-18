67fdfc53a5ab58740c6d2bb3ce7b667269b96c43
dam737 <nguyenvudam12345@gmail.com>
2026-02-18 15:07:06 +0700

    Update short.js

--- Newfolder/api/short.js
+++ Newfolder/api/short.js
@@ -1,26 +1,21 @@
-export default async function handler(req, res) {
+export default async function handler(req,res){
 
-  const { url } = req.query;
+ const url = req.query.url;
 
-  if (!url) {
-    res.status(400).json({ error: "missing url" });
-    return;
-  }
+ if(!url){
+   return res.json({error:"missing url"});
+ }
 
-  const token = "d754503b7998d34db1e601738a0934fca7dbd20a72fdd615ecabcf6a6919a14a";
+ const api =
+ "https://yeumoney.com/QL_api.php"+
+ "?token=d754503b7998d34db1e601738a0934fca7dbd20a72fdd615ecabcf6a6919a14a"+
+ "&format=text"+
+ "&url="+encodeURIComponent(url);
 
-  const api =
-    `https://yeumoney.com/QL_api.php?token=${token}&format=text&url=${encodeURIComponent(url)}`;
+ const r = await fetch(api);
+ const text = await r.text();
 
-  try {
-    const r = await fetch(api);
-    const text = await r.text();
-
-    res.status(200).json({
-      short: text.trim()
-    });
-
-  } catch (e) {
-    res.status(500).json({ error: "api failed" });
-  }
+ res.json({
+   short: text.trim()
+ });
 }
